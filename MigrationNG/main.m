cbedb9220045a7bd3ac4813d08f59450aa6d6ab8
mmasashi <mmasashi@gmail.com>
2011-11-02 00:30:59 +0900

    Initial Commit

---
+++ MigrationNG/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MigrationNG
+//
+//  Created by Miyazaki Masashi on 11/11/02.
+//  Copyright (c) 2011 mmasashi.jp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
